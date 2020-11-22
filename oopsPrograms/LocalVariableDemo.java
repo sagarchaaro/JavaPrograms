6351fb4e11562bcfac7de00db03173555d3ae055
Sagar Chakilala - Automation <sagarch.aaro@gmail.com>
2020-11-23 01:27:05 +0530

    Update LocalVariableDemo.java

--- oopsPrograms/LocalVariableDemo.java
+++ oopsPrograms/LocalVariableDemo.java
@@ -4,7 +4,7 @@ public class LocalVariableDemo {
 	public static void main(String[] args) {
 		int un=89;
 		
-		System.out.println(un);
+		System.out.println("Local Variable value is :"+un);
 		
 	}
 }

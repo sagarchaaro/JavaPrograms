9572efda76db6fc8b1752d4250340b56d3f5c70b
Sagar Chakilala <sagarch.aaro@gmail.com>
2020-11-23 02:27:44 +0530

    JavaFundamentals

---
+++ javaFundamentals/MultipleClasses.java
@@ -0,0 +1,26 @@
+package oct_23_2020;
+
+import java.lang.*;
+
+class Demo {
+	public static void main(String[] args) {
+		System.out.println("This is Demo Program");
+		int a=10;
+		int x=20;
+		System.out.println(a);
+		System.out.println(x);
+	}
+}
+
+class MNOP{
+	public static void main(String[] args) {
+		System.out.println("This is ABC Class");
+		int y=33, z=99;
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
+
+class XYZ{
+	
+}

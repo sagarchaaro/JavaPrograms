f6218cb944524d868b54e8ffc967859732141fa9
Sagar Chakilala - Automation <sagarch.aaro@gmail.com>
2020-11-23 01:35:12 +0530

    Update InstanceVariableDemo.java

--- oopsPrograms/InstanceVariableDemo.java
+++ oopsPrograms/InstanceVariableDemo.java
@@ -1,13 +1,13 @@
 package nov_05_2020;
 
 public class InstanceVariableDemo {
-	int s=90;
+	int var1=90;
 	
 	public static void main(String[] args) {
 		InstanceVariableDemo obj = new InstanceVariableDemo();
 		
-		System.out.println("Using Object Reference "+obj.s);//By using Object Reference
+		System.out.println("Using Object Reference "+obj.var1);//By using Object Reference
 		
-		System.out.println("Using Object "+new InstanceVariableDemo().s); //By using the Object
+		System.out.println("Using Object "+new InstanceVariableDemo().var1); //By using the Object
 	}
 }
